5732924e8ae5c7bf9b2275d720e4829e4e424adb
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-08-27 07:08:26 -0300

    feat: Creating DAO and Board service

---
+++ src/main/java/com/dio/gustavo/persistence/entity/BoardColumnKindEnum.java
@@ -0,0 +1,7 @@
+package com.dio.gustavo.persistence.entity;
+
+public enum BoardColumnKindEnum {
+
+    INITIAL, FINAL, CANCEL, PENDING
+
+}
---
+++ src/main/java/com/dio/gustavo/service/BoardService.java
@@ -0,0 +1,29 @@
+package com.dio.gustavo.service;
+
+import com.dio.gustavo.persistence.dao.BoardDAO;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public boolean delete(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        try {
+            if(!dao.exists(id)) {
+                return false;
+            }
+            dao.delete(id);
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+    }
+
+}
